7a6d707d5fd9a8729c3996f557ea555d7732909a
CDM-TECHNOLOGIE <ablainpierre@gmail.com>
2026-05-17 14:58:53 -0400

    Create SMS.py

---
+++ SMS.py
@@ -0,0 +1,26 @@
+==============================
+
+#🚧 CONFIGURATION TAKE SMS 🚧
+
+==============================
+
+NUMÉRO DU CIBLE DEV A-FBI 🌐
+
+target_number = "+509xxxxxxxx"
+sauvegarder_sms("+509xxxxxxxx", "Ton message ici", target_number)
+
+# ==============================  
+# PART 02  
+# ==============================  
+  
+```python  
+def sauvegarder_sms(numero_source, message, target_number):  
+    if numero_source == target_number:  
+        with open("sms_recus.txt", "a") as f:  
+            f.write(f"De \n")  
+            print("Message enregistré avec succès ! 📝")  
+  
+  
+  
+  
+Edem
